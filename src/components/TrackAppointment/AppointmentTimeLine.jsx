56b326a032d5d59187eff73fca062e812da9ac4d
Hariom Singh Rajput <hariommewada484@gmail.com>
2024-04-04 13:50:45 +0530

    Styled Track Appointment Page

--- src/components/TrackAppointment/AppointmentTimeLine.jsx
+++ src/components/TrackAppointment/AppointmentTimeLine.jsx
@@ -1,84 +1,90 @@
 import "../../stylesheets/TrackAppointment.css";
 import { appointStatusDsc } from "../../constant/appointmentStatus";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
 const AppointmentTimeLine = ({ data }) => {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="time-line-section">
-              <ul className="timeline timeline-horizontal">
-                <li className="timeline-item mb-5 position-relative">
-                  <div className="timeline-badge bg-white"></div>
-                  <div
-                    className="timeline-panel position-relative shadow rounded p-2"
-                    style={{ maxWidth: "220px" }}
-                  >
-                    <div>
-                      <h6>Payment - {data?.paymentStatus}</h6>
-                      <p className="form-text mb-0">
-                        {appointStatusDsc?.payment}
-                      </p>
-                    </div>
-                  </div>
-                </li>
+      <section>
+        <VerticalTimeline lineColor="var(--textLight)">
+          <VerticalTimelineElement
+            iconStyle={{
+              background: "var(--primaryColor)",
+              color: "white",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              fontSize: "1.25rem",
+            }}
+            icon={<i class="fa-solid fa-money-check-dollar"></i>}
+          >
+            <h3 className="timeline-heading">
+              Payment - {data?.paymentStatus}
+            </h3>
+            <p className="timeline-description">{appointStatusDsc?.payment}</p>
+          </VerticalTimelineElement>
 
-                <li className="timeline-item mb-5 position-relative">
-                  <div className="timeline-badge bg-white"></div>
-                  <div
-                    className="timeline-panel position-relative shadow rounded p-2"
-                    style={{ maxWidth: "220px" }}
-                  >
-                    <div>
-                      <h6>Appointment - {data?.status}</h6>
-                      <p className="form-text mb-0">
-                        {appointStatusDsc.appointment[data?.status]}
-                      </p>
-                    </div>
-                  </div>
-                </li>
+          <VerticalTimelineElement
+            iconStyle={{
+              background: "var(--primaryColor)",
+              color: "white",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              fontSize: "1.25rem",
+            }}
+            icon={<i class="fa-solid fa-calendar-check"></i>}
+          >
+            <h3 className="timeline-heading">Appointment - {data?.status}</h3>
+            <p className="timeline-description">
+              {appointStatusDsc.appointment[data?.status]}
+            </p>
+          </VerticalTimelineElement>
 
-                <li className="timeline-item mb-5 position-relative">
-                  <div className="timeline-badge bg-white"></div>
-                  <div
-                    className="timeline-panel position-relative shadow rounded p-2"
-                    style={{ maxWidth: "220px" }}
-                  >
-                    <div>
-                      <h6>
-                        Follow-up Date -{" "}
-                        {data?.followUp ? data?.followUp : "Not Scheduled Yet"}
-                      </h6>
-                      <p className="form-text mb-0">
-                        {data?.followUp && appointStatusDsc.followUpDate}
-                      </p>
-                    </div>
-                  </div>
-                </li>
+          <VerticalTimelineElement
+            iconStyle={{
+              background: "var(--primaryColor)",
+              color: "white",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              fontSize: "1.25rem",
+            }}
+            icon={<i class="fa-solid fa-list"></i>}
+          >
+            <h3 className="timeline-heading">
+              Follow-up Date -{" "}
+              {data?.followUp ? data?.followUp : "Not Scheduled Yet"}
+            </h3>
+            <p className="timeline-description">
+              {data?.followUp && appointStatusDsc.followUpDate}
+            </p>
+          </VerticalTimelineElement>
 
-                <li className="timeline-item mb-5 position-relative">
-                  <div className="timeline-badge bg-white"></div>
-                  <div
-                    className="timeline-panel position-relative shadow rounded p-2"
-                    style={{ maxWidth: "220px" }}
-                  >
-                    <div>
-                      <h6>Prescription - {data?.prescriptionStatus}</h6>
-                      <p className="form-text mb-0">
-                        {
-                          appointStatusDsc.prescriptionStatus[
-                            data?.prescriptionStatus
-                          ]
-                        }
-                      </p>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+          <VerticalTimelineElement
+            iconStyle={{
+              background: "var(--primaryColor)",
+              color: "white",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              fontSize: "1.25rem",
+            }}
+            icon={<i class="fa-solid fa-file-lines"></i>}
+          >
+            <h3 className="timeline-heading">
+              Prescription - {data?.prescriptionStatus}
+            </h3>
+            <p className="timeline-description">
+              {appointStatusDsc.prescriptionStatus[data?.prescriptionStatus]}
+            </p>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      </section>
     </>
   );
 };
--- src/components/TrackAppointment/TrackDetailPage.jsx
+++ src/components/TrackAppointment/TrackDetailPage.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import img from '../../images/user.png';
+import React from "react";
+import img from "../../images/user.png";
 import "../../stylesheets/TrackAppointment.css";
-import { FaCalendarCheck, FaRegClock, FaArrowAltCircleLeft } from "react-icons/fa";
-import 'react-vertical-timeline-component/style.min.css';
-import AppointmentTimeLine from './AppointmentTimeLine';
-import { appointStatusDsc } from '../../constant/appointmentStatus';
-import moment from 'moment';
-import { Button } from 'antd';
+import {
+  FaCalendarCheck,
+  FaRegClock,
+  FaArrowAltCircleLeft,
+} from "react-icons/fa";
+import "react-vertical-timeline-component/style.min.css";
+import AppointmentTimeLine from "./AppointmentTimeLine";
+import { appointStatusDsc } from "../../constant/appointmentStatus";
+import moment from "moment";
+import { Button } from "antd";
 
 const TrackDetailPage = ({ data, setShowInfo }) => {
   const patinetFirstName = data?.patient?.firstName
@@ -33,13 +37,13 @@ const TrackDetailPage = ({ data, setShowInfo }) => {
       </div>
       <div className="container track-detail rounded">
         <div className="row">
+          {/* Profile Card */}
           <div className="col-md-4">
             <div className="d-flex flex-column gap-4 justify-content-around">
-              <div className="card shadow-sm p-3 text-center shadow border-0 bg-success">
+              {/* Patient Card */}
+              <div className="card p-3 text-center profile-card">
                 <div className="text-white">
-                  <h6 className="text-start text-primary text-uppercase text-black">
-                    Patient
-                  </h6>
+                  <h6 className="text-start label">Patient</h6>
                   <div className="d-flex gap-2">
                     <div className="img-div">
                       {data?.patient?.img ? (
@@ -49,10 +53,10 @@ const TrackDetailPage = ({ data, setShowInfo }) => {
                       )}
                     </div>
                     <div className="text-start">
-                      <h6 className="mb-0 text-white">
+                      <h6 className="name">
                         {patinetFirstName + " " + patinetLastName}
                       </h6>
-                      <p className="form-text text-white">
+                      <p className="address">
                         {data?.patient?.address &&
                           data?.patient?.address + "," + data?.patient?.city &&
                           data?.patient?.city}
@@ -65,11 +69,10 @@ const TrackDetailPage = ({ data, setShowInfo }) => {
                 </div>
               </div>
 
-              <div className=" card shadow-sm p-3 text-center shadow border-0 bg-danger text-white">
+              {/* Doctor Card */}
+              <div className="card p-3 text-center profile-card">
                 <div>
-                  <h6 className="text-start text-uppercase text-black">
-                    Doctor
-                  </h6>
+                  <h6 className="text-start label">Doctor</h6>
                   <div className="d-flex gap-2">
                     <div className="img-div">
                       {data?.doctor?.img ? (
@@ -79,12 +82,10 @@ const TrackDetailPage = ({ data, setShowInfo }) => {
                       )}
                     </div>
                     <div className="text-start">
-                      <h6 className="mb-0 text-white">
+                      <h6 className="name">
                         {doctorFirstName + " " + doctorLastName}
                       </h6>
-                      <p className="form-text text-white">
-                        {data?.doctor?.designation}
-                      </p>
+                      <p className="address">{data?.doctor?.designation}</p>
                     </div>
                   </div>
                 </div>
@@ -92,35 +93,41 @@ const TrackDetailPage = ({ data, setShowInfo }) => {
             </div>
           </div>
 
+          {/* Appointment Details */}
           <div className="col-md-8">
-            <h4 className="text-center text-capitalized">
-              Status : {data?.status}
-            </h4>
-            <p className="px-5 form-text text-center">
-              {appointStatusDsc.appointment[data?.status]}
-            </p>
+            <div className="detail-card">
+              <h4 className="text-start text-capitalized">
+                Status : {data?.status}
+              </h4>
+              <p className="text-secondary text-start my-2">
+                {appointStatusDsc.appointment[data?.status]}
+              </p>
 
-            <div className="d-flex justify-content-center mt-4">
-              <div className="text-center">
-                <h5>Meeting Schedule: </h5>
-                <div className="text-secondary text-start">
-                  <FaCalendarCheck className="me-2" />
-                  Appointment Date :{" "}
-                  {moment(data?.scheduleDate).format("MMM Do YY")}
-                </div>
-                <div className="text-secondary text-start">
-                  <FaRegClock className="me-2" />
-                  Appointment Time : {data?.scheduleTime}
+              <div className="d-flex justify-content-start mt-4">
+                <div className="text-start">
+                  <h4>Meeting Schedule</h4>
+                  <div className="text-secondary text-start my-2">
+                    <FaCalendarCheck className="me-2" />
+                    Appointment Date :{" "}
+                    {moment(data?.scheduleDate).format("MMM Do YY")}
+                  </div>
+                  <div className="text-secondary text-start my-2">
+                    <FaRegClock className="me-2" />
+                    Appointment Time : {data?.scheduleTime}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div>
-          <h4 className="text-center mt-4 text-uppercase">
-            Appointment Timeline:{" "}
-          </h4>
+        <div style={{ marginTop: "5rem" }}>
+          <div className="text-center mb-5">
+            <div className="section-title mb-3">
+              <h2>Appointment Timeline</h2>
+              <p>Here is the timeline of your appointment</p>
+            </div>
+          </div>
           <AppointmentTimeLine data={data} />
         </div>
       </div>
