d4bac192d5f900672e6223ddc10dd8d4343c1c27
Hariom Singh Rajput <hariommewada484@gmail.com>
2024-04-05 14:16:06 +0530

    Now only patient can review doctor

--- src/components/Doctor/DoctorProfile/DoctorProfile.jsx
+++ src/components/Doctor/DoctorProfile/DoctorProfile.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import Footer from '../../Shared/Footer/Footer'
+import React from "react";
+import Footer from "../../Shared/Footer/Footer";
 import "../../../stylesheets/doctorStylesheets/DoctorProfile.css";
-import { useParams } from 'react-router-dom';
-import Header from '../../Shared/Header/Header';
-import SubHeader from '../../Shared/SubHeader';
-import { useGetDoctorQuery } from '../../../redux/api/doctorApi';
-import { Empty, message } from 'antd';
-import { Tabs } from 'antd';
-import OverView from './OverView';
-import Location from './Location';
-import Review from './Review';
-import Availability from './Availability';
-import DoctorProfileView from '../SearchDoctor/DoctorProfileView';
+import { useParams } from "react-router-dom";
+import Header from "../../Shared/Header/Header";
+import SubHeader from "../../Shared/SubHeader";
+import { useGetDoctorQuery } from "../../../redux/api/doctorApi";
+import { Empty, message } from "antd";
+import { Tabs } from "antd";
+import OverView from "./OverView";
+import Location from "./Location";
+import Review from "./Review";
+import Availability from "./Availability";
+import DoctorProfileView from "../SearchDoctor/DoctorProfileView";
+import useAuthCheck from "../../../redux/hooks/useAuthCheck";
 
 const DoctorProfile = () => {
+  const { role } = useAuthCheck();
+
   const { id } = useParams();
   const { data, isLoading, isError } = useGetDoctorQuery(id);
   let content = null;
@@ -24,6 +27,24 @@ const DoctorProfile = () => {
     content = <DoctorProfileView data={data} />;
 
   const items = [
+    {
+      key: "1",
+      label: "Overview",
+      children: <OverView />,
+    },
+    {
+      key: "2",
+      label: "Locations",
+      children: <Location />,
+    },
+    {
+      key: "3",
+      label: "Availability",
+      children: <Availability />,
+    },
+  ];
+
+  const authItems = [
     {
       key: "1",
       label: "Overview",
@@ -58,11 +79,11 @@ const DoctorProfile = () => {
         style={{ marginBottom: "4rem", marginTop: "6rem" }}
       >
         {content}
-        <div
-          className="p-4 rounded"
-          style={{ marginBottom: "3rem"}}
-        >
-          <Tabs defaultActiveKey="1" items={items} />
+        <div className="p-4 rounded" style={{ marginBottom: "3rem" }}>
+          <Tabs
+            defaultActiveKey="1"
+            items={role === "patient" ? authItems : items}
+          />
         </div>
       </div>
       <Footer />
