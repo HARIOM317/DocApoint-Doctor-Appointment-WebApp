81231e90c0cc4ac07ad803eed606610565ee14f1
Hariom Singh Rajput <hariommewada484@gmail.com>
2024-04-16 14:08:20 +0530

    Made About, Services and Contact Page Responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,8 +11,10 @@ import ContactSupport from "../animations/contact_us.json";
 import ContactPageHeader from "./ContactPageHeader";
 
 const Contact = () => {
-  const [contact, { isLoading, isError, error, isSuccess }] =
-    useContactMutation();
+  const [
+    contact,
+    { isLoading, isError, error, isSuccess },
+  ] = useContactMutation();
   const { register, handleSubmit, reset } = useForm({});
   const onSubmit = (data) => {
     contact(data);
@@ -37,104 +39,89 @@ const Contact = () => {
       <ContactPageHeader />
 
       <section id="contact" className="contact">
-        <div
-          className="container"
-          style={{ marginTop: "50px", marginBottom: "50px" }}
-        >
+        <div className="container" style={{ marginBottom: "50px" }}>
           <div className="row">
-            <div className="col-12 col-lg-10 mx-auto">
-              <div className="row">
-                {/* Left Side Part */}
-                <div className="col-12 col-lg-5">
-                  <figure>
-                    <Lottie
-                      loop={true}
-                      animationData={ContactSupport}
-                      className="lottie-animation"
+            {/* Left Side Part */}
+            <div className="col-12 col-lg-5">
+              <figure>
+                <Lottie
+                  loop={true}
+                  animationData={ContactSupport}
+                  className="lottie-animation"
+                />
+              </figure>
+            </div>
+
+            {/* Right Side Part */}
+            <div className="col-12 col-lg-7">
+              <div className="mb-5 p-2 rounded">
+                <form
+                  className="row form-row"
+                  onSubmit={handleSubmit(onSubmit)}
+                >
+                  {/* first row */}
+                  <div className="col-12 col-lg-6 contact-input-field">
+                    <input
+                      required
+                      {...register("firstName")}
+                      className="form-control"
+                      placeholder="First Name"
                     />
-                  </figure>
-                </div>
-
-                {/* Right Side Part */}
-                <div className="col-12 col-lg-7">
-                  <div className="mb-5 p-2 rounded">
-                    <form
-                      className="row form-row"
-                      onSubmit={handleSubmit(onSubmit)}
-                    >
-                      {/* first row */}
-                      <div className="row">
-                        <div className="col-12 col-lg-6 contact-input-field">
-                          <input
-                            required
-                            {...register("firstName")}
-                            className="form-control"
-                            placeholder="First Name"
-                          />
-                        </div>
-
-                        <div className="col-12 col-lg-6 contact-input-field">
-                          <input
-                            required
-                            {...register("lastName")}
-                            className="form-control"
-                            placeholder="Last Name"
-                          />
-                        </div>
-                      </div>
-
-                      {/* second row */}
-                      <div className="row">
-                        <div className="col-12 contact-input-field">
-                          <input
-                            required
-                            {...register("email")}
-                            type="email"
-                            className="form-control"
-                            placeholder="Email"
-                          />
-                        </div>
-                      </div>
-
-                      {/* third row */}
-                      <div className="row">
-                        <div className="col-12 contact-input-field">
-                          <input
-                            required
-                            {...register("subject")}
-                            className="form-control"
-                            placeholder="Enter your subject"
-                          />
-                        </div>
-                      </div>
-
-                      {/* forth row */}
-                      <div className="row">
-                        <div className="col-12 contact-input-field">
-                          <textarea
-                            required
-                            {...register("text")}
-                            className="form-control mb-3"
-                            cols="30"
-                            rows="10"
-                            placeholder="enter your message"
-                          />
-                        </div>
-                      </div>
-
-                      {/* Submit Button */}
-                      <div className="text-center">
-                        <button
-                          disabled={isLoading}
-                          type="submit"
-                          className="appointment-btn"
-                        >
-                          Send Message
-                        </button>
-                      </div>
-                    </form>
                   </div>
-                </div>
+
+                  <div className="col-12 col-lg-6 contact-input-field">
+                    <input
+                      required
+                      {...register("lastName")}
+                      className="form-control"
+                      placeholder="Last Name"
+                    />
+                  </div>
+
+                  {/* second row */}
+                  <div className="col-12 contact-input-field">
+                    <input
+                      required
+                      {...register("email")}
+                      type="email"
+                      className="form-control"
+                      placeholder="Email"
+                    />
+                  </div>
+
+                  {/* third row */}
+                  <div className="col-12 contact-input-field">
+                    <input
+                      required
+                      {...register("subject")}
+                      className="form-control"
+                      placeholder="Enter your subject"
+                    />
+                  </div>
+
+                  {/* forth row */}
+                  <div className="col-12 contact-input-field">
+                    <textarea
+                      required
+                      {...register("text")}
+                      className="form-control mb-3"
+                      cols="30"
+                      rows="10"
+                      placeholder="enter your message"
+                    />
+                  </div>
+
+                  {/* Submit Button */}
+                  <div className="text-center">
+                    <button
+                      disabled={isLoading}
+                      type="submit"
+                      className="button"
+                    >
+                      Send Message
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
