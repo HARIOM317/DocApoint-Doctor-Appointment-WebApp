e6ca3c86e3bc59449158ea3ee85a2e3360e35088
Hariom Singh Rajput <hariommewada484@gmail.com>
2024-04-30 02:01:21 +0530

    Created A new file to show patient medical history

---
+++ src/components/Doctor/PatientHistory/PatientMedicalHistory.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DashboardLayout from "../DashboardLayout/DashboardLayout";
+
+const PatientMedicalHistory = () => {
+  return (
+    <DashboardLayout>
+      <div>Patient Medical History</div>
+    </DashboardLayout>
+  );
+};
+
+export default PatientMedicalHistory;
